db85512b71fa99cc843ec31db47bbb75fe1567d2
britt frenkel <brittfrenkel19@gmail.com>
2020-07-07 21:09:24 +0300

    joypad poc finished

---
+++ src/joypad/make.bat
@@ -0,0 +1,3 @@
+C:\gbdk\gbdk\bin\lcc -Wa-l -Wl-m -Wl-j -DUSE_SFR_FOR_REG -c -o main.o main.c 
+C:\gbdk\gbdk\bin\lcc -Wa-l -Wl-m -Wl-j -DUSE_SFR_FOR_REG -c -o joypad.o joypad.c 
+C:\gbdk\gbdk\bin\lcc -Wa-l -Wl-m -Wl-j -DUSE_SFR_FOR_REG -o program.gb main.o joypad.o
